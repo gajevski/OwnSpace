ac943469dd30e2d5e5207f25db962f9691f01152
gajevski <mikolajxgajewski@gmail.com>
2023-10-02 13:03:28 +0200

    Add authform component

---
+++ app/(site)/components/AuthForm.tsx
@@ -0,0 +1,77 @@
+'use client';
+
+import { signIn, useSession } from 'next-auth/react';
+import { useEffect, useState } from 'react';
+import { BsGithub } from 'react-icons/bs';
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
+import { useRouter } from "next/navigation";
+
+import AuthSocialButton from './AuthSocialButton';
+
+const AuthForm = () => {
+    const session = useSession();
+    const router = useRouter();
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+        if (session?.status === 'authenticated') {
+            router.push('/welcome')
+        }
+    }, [session?.status, router]);
+
+    const socialAction = (action: string) => {
+        setIsLoading(true);
+
+        signIn(action, { redirect: false })
+            .then((callback) => {
+                if (callback?.error) {
+                    console.error('Invalid credentials!');
+                }
+
+                if (callback?.ok) {
+                    router.push('/welcome')
+                }
+            })
+            .finally(() => setIsLoading(false));
+    }
+
+    return (
+        <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+            <div
+                className="
+        bg-white
+          px-4
+          py-8
+          shadow
+          sm:rounded-lg
+          sm:px-10
+        "
+            >
+
+                <div className="mt-6">
+
+                    <div className="mt-6 flex gap-2">
+                        <AuthSocialButton
+                            icon={BsGithub}
+                            onClick={() => socialAction('github')}
+                        />
+                    </div>
+                </div>
+                <div
+                    className="
+            flex 
+            gap-2 
+            justify-center 
+            text-sm 
+            mt-6 
+            px-2 
+            text-gray-500
+          "
+                >
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default AuthForm;
