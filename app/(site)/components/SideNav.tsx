651491306fe1bbc60ebe27cbe19a9633867876c9
gajevski <mikolajxgajewski@gmail.com>
2023-10-03 14:52:49 +0200

    Add games link

--- app/(site)/components/SideNav.tsx
+++ app/(site)/components/SideNav.tsx
@@ -4,8 +4,13 @@ const SideNav = () => {
         <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
           <ul className="space-y-2 font-medium">
             <li>
-              <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+              <a href="/welcome" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                 <span className="ml-3">Welcome</span>
+              </a>
+            </li>
+            <li>
+              <a href="/games" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                <span className="ml-3">Games</span>
               </a>
             </li>
             <li>
