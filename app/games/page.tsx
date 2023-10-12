d83b2652d016c233661a88b10dfc0de80426b2c0
gajevski <mikolajxgajewski@gmail.com>
2023-10-12 11:55:23 +0200

    Add modal to add a new game

--- app/games/page.tsx
+++ app/games/page.tsx
@@ -32,9 +32,48 @@ export default function Games() {
     return (
         <div className="h-full flex flex-col">
             <div className="flex items-center justify-end">
-                <button className="btn btn-accent" onClick={addGame}>Add game</button>
+                {/* <button className="btn btn-accent" onClick={addGame}>Add game</button> */}
+                <button className="btn btn-accent" onClick={() => document.getElementById('add_game_modal').showModal()}>Add game</button>
             </div>
             <UserGames />
+            <dialog id="add_game_modal" className="modal">
+                <div className="modal-box flex flex-col items-center">
+                    <h3 className="font-bold text-lg mb-8">Add a new game</h3>
+                    <div className="form-control w-full max-w-xs mb-4">
+                        <label className="label">
+                            <span className="label-text">Image URL</span>
+                        </label>
+                        <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+                    </div>
+                    <div className="form-control w-full max-w-xs mb-4">
+                        <label className="label">
+                            <span className="label-text">Title</span>
+                        </label>
+                        <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+                    </div>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label">
+                            <span className="label-text">Description</span>
+                        </label>
+                        <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+                    </div>
+                    <div className="modal-action">
+                        <button className="btn btn-outline" onClick={() => window.add_game_modal.close()}>
+                            Close
+                        </button>
+                        <button
+                            type="button"
+                            className="btn btn-info"
+                        >
+                            Add
+                        </button>
+                    </div>
+                </div>
+                <form method="dialog" className="modal-backdrop">
+                    <button>close</button>
+                </form>
+            </dialog>
+
         </div>
     );
 }
