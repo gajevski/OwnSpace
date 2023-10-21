a6056ac05cef6f74f962f15e6fd12f4a221b8175
gajevski <mikolajxgajewski@gmail.com>
2023-10-21 21:02:41 +0200

    Fix modal typing error

--- app/games/page.tsx
+++ app/games/page.tsx
@@ -2,6 +2,7 @@
 
 import Link from "next/link";
 import { useState, useEffect } from "react";
+import "../models/window";
 
 export default function Games() {
     const [games, setGames] = useState([]);
