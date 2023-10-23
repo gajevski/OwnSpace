268e4e75c3e29299872bf5c4b2004dba9f36ca9d
gajevski <mikolajxgajewski@gmail.com>
2023-10-23 21:24:53 +0200

    Refactor gameId API typing

--- app/api/games/[gameId]/route.ts
+++ app/api/games/[gameId]/route.ts
@@ -3,10 +3,15 @@ import { NextResponse } from "next/server";
 
 import prisma from "@/app/libs/prismadb";
 import { NextApiRequest, NextApiResponse } from "next";
+import { Game } from "@/app/models/game";
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
-  const id = req.url.split("games/")[1];
-  const gameId = String(id);
+interface GameResponse {
+  game: Game;
+}
+
+export async function GET(req: NextApiRequest, res: NextApiResponse<NextResponse>) {
+  const id: string = req.url?.split("games/")?.[1] ?? '';
+  const gameId: string = String(id);
 
   try {
     const currentUser = await getCurrentUser();
@@ -15,7 +20,7 @@ export async function GET(req: NextApiRequest, res: NextApiResponse) {
       return new NextResponse('Unauthorized', { status: 400 });
     }
 
-    const game = await prisma.game.findUnique({
+    const game: Game | null = await prisma.game.findUnique({
       where: {
         id: gameId,
       },
@@ -25,10 +30,12 @@ export async function GET(req: NextApiRequest, res: NextApiResponse) {
       return new NextResponse('Game not found', { status: 404 });
     }
 
+    const gameResponse: GameResponse = {
+      game,
+    };
+
     return new NextResponse(
-      JSON.stringify({
-        game,
-      })
+      JSON.stringify(gameResponse)
     );
   } catch (error) {
     console.error('Error fetching game details:', error);
