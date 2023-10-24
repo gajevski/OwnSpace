cacc506b4f0bd7567325f64250757b8809ac8b66
gajevski <mikolajxgajewski@gmail.com>
2023-10-24 19:53:35 +0200

    Fix types in SideNav

--- app/(site)/components/SideNav.tsx
+++ app/(site)/components/SideNav.tsx
@@ -1,4 +1,6 @@
-const SideNav = () => {
+import React from 'react';
+
+const SideNav: React.FC<{}> = () => {
     return (
         <aside id="default-sidebar" className="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
         <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-base-300">
