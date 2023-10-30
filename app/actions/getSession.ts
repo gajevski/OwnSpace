542b7334bfdb811796df0bb91875343b3571e25b
gajevski <mikolajxgajewski@gmail.com>
2023-10-30 22:17:12 +0100

    Add missing types to getSession

--- app/actions/getSession.ts
+++ app/actions/getSession.ts
@@ -1,7 +1,7 @@
-import { getServerSession } from "next-auth";
+import { getServerSession, Session } from "next-auth";
 
 import { options } from "@/app/api/auth/[...nextauth]/options";
 
-export default async function getSession() {
+export default async function getSession(): Promise<Session | null> {
   return await getServerSession(options);
 }
