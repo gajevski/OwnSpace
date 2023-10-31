8780b18bd9275dbb44f45ac6928386f792aa7fdd
gajevski <mikolajxgajewski@gmail.com>
2023-10-31 23:12:39 +0100

    Refactor auth route file

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,10 @@
 import NextAuth from "next-auth";
+import { NextApiHandler } from 'next'
+import { NextApiRequest, NextApiResponse } from "next";
 import { options } from "./options";
 
-const handler = NextAuth(options);
+const handler: NextApiHandler = async (req: NextApiRequest, res: NextApiResponse) => {
+    return await NextAuth(req, res, options);
+};
 
-export { handler as GET, handler as POST }
+export default handler;
